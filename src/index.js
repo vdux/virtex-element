6b5a63d1971c8a40d61e3e723d273aa243b6dfb9
Andrew Shaffer <darawk@gmail.com>
2015-12-08 10:41:54 -0800

    update deps

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import {element as _element} from 'virtex'
 import EvStore from 'ev-store'
 import events from './events'
 import {string as toStyle} from 'to-style'
-import forEach from 'foreach'
+import forEach from '@micro-js/foreach'
+import isObject from '@micro-js/is-object'
+import keychord from '@micro-js/keychord'
 
 /**
  * virtex-element
@@ -15,49 +17,49 @@ import forEach from 'foreach'
 function element (tag, attrs) {
   // Only apply sugar to native elements
   if (typeof tag === 'string' && attrs) {
-    forEach(attrs, (val, key) => attrs[key] = sugar(key, val))
+    forEach((val, key) => attrs[key] = sugar(val, key), attrs)
   }
 
   return _element.apply(null, arguments)
 }
 
-function sugar (name, value) {
-  return styleSugar(name, eventSugar(name, focusSugar(name, classSugar(name, value))))
+function sugar (value, name) {
+  return styleSugar(eventSugar(focusSugar(classSugar(value, name), name), name), name)
 }
 
-function styleSugar (name, value) {
-  if (name === 'style' && typeof value === 'object') {
+function styleSugar (value, name) {
+  if (name === 'style' && isObject(value)) {
     return toStyle(value)
   }
 
   return value
 }
 
-function eventSugar (name, value) {
+function eventSugar (value, name) {
   return events.hasOwnProperty(name)
     ? bindEvent(events[name], value)
     : value
 }
 
-function focusSugar (name, value) {
+function focusSugar (value, name) {
   return name === 'focused'
     ? focusElement
     : value
 }
 
-function classSugar (name, value) {
+function classSugar (value, name) {
   if (name === 'class') {
     if (Array.isArray(value)) {
       return value.join(' ')
-    } else if (typeof value === 'object') {
+    } else if (isObject(value)) {
       let str = ''
 
-      forEach(value, (val, key) => {
+      forEach((val, key) => {
         if (val) {
           if (str) str += ' '
           str += key
         }
-      })
+      }, value)
 
       return str
     }
@@ -67,9 +69,27 @@ function classSugar (name, value) {
 }
 
 function bindEvent (eventName, fn) {
-  return Array.isArray(fn)
-    ? fn.map(f => bindEvent(eventName, f))
-    : node => EvStore(node)[eventName] = fn
+  return node => EvStore(node)[eventName] = createHandler(eventName, fn)
+}
+
+function createHandler (eventName, fn) {
+  if (isObject(fn) && isKeyboardEvent(eventName)) {
+    return e => {
+      var chord = keychord(e)
+      if (fn[chord]) {
+        var f = fn[chord]
+        return Array.isArray(f) ? f.map(f => f(e)) : f(e)
+      }
+    }
+  } else if (Array.isArray(fn)) {
+    return e => fn.map(f => f(e))
+  } else {
+    return fn
+  }
+}
+
+function isKeyboardEvent (eventName) {
+  return ['keydown', 'keypress', 'keyup'].indexOf(eventName) !== -1
 }
 
 function focusElement (node) {
